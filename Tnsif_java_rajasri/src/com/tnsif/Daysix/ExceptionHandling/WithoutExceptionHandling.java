31f8f26879536b16ba149c500cc9c122cece12ee
sri <sri@DESKTOP-G4CSFSE>
2025-10-08 21:30:28 +0530

    This is a java program

---
+++ Tnsif_java_rajasri/src/com/tnsif/Daysix/ExceptionHandling/WithoutExceptionHandling.java
@@ -0,0 +1,10 @@
+package com.tnsif.Daysix.ExceptionHandling;
+
+public class WithoutExceptionHandling {
+
+	public static void main(String[] args) {
+		System.out.println("the program continues....");
+		int data=100/0;
+	}
+	
+}
