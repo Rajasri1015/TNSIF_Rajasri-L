aef1bae6ebb5d3842083743cadcae30038fb1b19
sri <sri@DESKTOP-G4CSFSE>
2025-10-09 09:49:36 +0530

    This is java program

---
+++ Tnsif_java_rajasri/src/com/tnsif/Daythree/packagesandaccessmodifiers/PackageExample.java
@@ -0,0 +1,11 @@
+package com.tnsif.Daythree.packagesandaccessmodifiers;
+
+public class PackageExample {
+
+	
+	public void display() {
+		System.out.println("Displaying PackageExample class");
+	}
+	
+}
+
