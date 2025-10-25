505c97737c03161badd4cbdfe3d05f5ae5b77a06
sri <sri@DESKTOP-G4CSFSE>
2025-10-25 15:44:56 +0530

    This is a java program

---
+++ Tnsif_java_rajasri/src/com/tnsif/Dayten/DisbaledDemo.java
@@ -0,0 +1,37 @@
+package com.tnsif.Dayten;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Disabled;
+
+
+public class DisbaledDemo {
+	
+	
+	
+	@Test
+	 void test1()
+	{
+	System.out.println("Test 1");
+		
+		
+	}
+	
+	@Disabled
+	
+	@Test
+	 void test2()
+	{
+	System.out.println("Test 2");
+		
+		
+	}
+	
+	@Test
+	 void test3()
+	{
+	System.out.println("Test 3");
+		
+		
+	}
+
+}
---
+++ Tnsif_java_rajasri/src/com/tnsif/Dayten/MyfirstTest.java
@@ -0,0 +1,17 @@
+package com.tnsif.Dayten;
+
+import org.junit.jupiter.api.Test;
+
+public class MyfirstTest {
+	
+	
+	@Test //basic testing annotation
+	void display()
+	{
+		System.out.println("Hello World");
+	}
+	
+	
+
+}
+
