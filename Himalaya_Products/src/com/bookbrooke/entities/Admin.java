9eb014c0117711aadaf3140b4ab6277b0e5661b3
Rajasri <161140822+Rajasri1015@users.noreply.github.com>
2025-11-08 15:39:39 +0530

    Delete Himalaya_Products directory

--- Himalaya_Products/src/com/bookbrooke/entities/Admin.java
+++
@@ -1,7 +0,0 @@
-package com.bookbrooke.entities;
-
-public class Admin extends User {
-    public Admin(int id, String name, String email) {
-        super(id, name, email);
-    }
-}
--- Himalaya_Products/src/module-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * 
- */
-/**
- * 
- */
-module Himalaya_Products {
-}
